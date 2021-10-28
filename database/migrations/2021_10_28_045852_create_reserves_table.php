7645177f708531c2f4d4873f7e11106e258ef9bd
hirata3200 <k1420kei@gmail.com>
2021-10-28 15:12:54 +0900

    hotel

--- database/migrations/2021_10_28_045852_create_reserves_table.php
+++ database/migrations/2021_10_28_045852_create_reserves_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateReserveTable extends Migration
+class CreateReservesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,12 @@ class CreateReserveTable extends Migration
      */
     public function up()
     {
-        Schema::create('reserve', function (Blueprint $table) {
+        Schema::create('reserves', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('address');
-            $table->string('tel');
+            $table->integer('users_id');
+            $table->string('number');
+            $table->dateTime('check_in');
+            $table->dateTime('check_out');
             $table->timestamps();
         });
     }
@@ -29,6 +30,6 @@ class CreateReserveTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('reserve');
+        Schema::dropIfExists('reserves');
     }
 }
